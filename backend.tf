c1bd6dfc32de04c19608c79accda748db50c13e4
olu13 <69245639+olu13@users.noreply.github.com>
2022-06-17 14:59:26 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "terraform-bucket-dev"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table1"
   }
 }
