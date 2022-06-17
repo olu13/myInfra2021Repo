27b4623d0eb63f9cdff76659eca10655ff6098c8
olu13 <69245639+olu13@users.noreply.github.com>
2022-06-17 15:20:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "launch-wizard-12" 
+    default     = "launch-wizard-112" 
 }
 
 variable "tag_name" { 
